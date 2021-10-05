97fe663c3cfab33a3209a20bb5956330bcc62334
Raul <raul.turc@stud.ubbcluj.ro>
2021-10-05 07:59:59 +0300

    Homework

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ def test_get_largest_prime_below():
   assert get_largest_prime_below(4) is 3
 
 def get_age_in_days(birthday):
-  from datetime import datetime
-  number_of_days_since_birthday = (datetime.utcnow() - birthday).days
+  from datetime import datetime 
+  number_of_days_since_birthday = (datetime.utcnow() - birthday).days 
   return number_of_days_since_birthday  
 
 def test_get_age_in_days():
@@ -34,6 +34,23 @@ def test_get_age_in_days():
   assert get_age_in_days(12/12/2012) is 3217
   assert get_age_in_days(6/6/2006) is 5598
   assert get_age_in_days(1/1/2001) is 7580
+
+def is_palindrome(n):
+  ok=False
+  cn=n
+  m=0
+  if n<10:
+    return ok
+  while cn>0:
+    m= m*10 + (cn%10)
+    cn//=10
+  if m == n:
+    ok=True
+  return ok
+
+def test_is_palindrome():
+  assert is_palindrome(1221) is 1
+  assert is_palindrome(232) is 1
    
 
 def main():
@@ -41,6 +58,7 @@ def main():
 Meniu:
 1. Ultimul numar prim mai mic decat un numar dat
 2. Determinati varsta unei persoane dupa data nasterii introdusa sub forma:DD/MM/YYYY
+3. Determinati daca un numar este palindrom sau nu
 x. Inchideti programul
   """)
   option = input("Selectati o optiune: ")
@@ -61,6 +79,11 @@ x. Inchideti programul
       birthday = datetime.strptime(datestr, "%d/%m/%Y")
       day=get_age_in_days(birthday)
       print(day,'zile')
+      shouldRun=True
+    elif option == "3":
+      n=int(input("Introduceti un numar: "))
+      m=is_palindrome(n)
+      print(m)
       shouldRun=True
     if shouldRun == True:
       option = input("Selectati o optiune: ")
